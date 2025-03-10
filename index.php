9d82aa78e559338c0323da1a7cbe33e219573f4b
Salsabila Grima <salsabilagrima@gmail.com>
2025-03-10 14:00:52 +0800

    membuat file prodi.php

--- index.php
+++ index.php
@@ -7,5 +7,24 @@
 </head>
 <body>
     <h1>DATA MAHASISWA</h1>
+    <br>
+    <table border="1" cell spassing>
+        <thead>
+            <th>No</th>
+            <th>Nim</th>
+            <th>Nama</th>
+        </thead>
+        </tbody>
+            <tr>
+                <td>1</td>
+                <td>E020323052</td>
+                <td>Salsabila Grima</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>E020323056</td>
+                <td>Zukimi</td>
+            </tr>
+        </tbody>
 </body>
 </html>
